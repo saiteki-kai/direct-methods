c1d5c3b110659c0b354ee8986bda95f4e9d724c0
lordfighterx <lordfighterx@gmail.com>
2021-06-03 18:08:36 +0200

    refactoring

--- matlab/main.m
+++ matlab/main.m
@@ -1,17 +1,31 @@
 clear;
 close all;
 
-filename = "0_GT01R.mat";
+os = 'linux';
+names = ["0_GT01R.mat","1_TSC_OPF_1047.mat", ...
+    "2_ns3Da.mat","3_nd24k.mat","4_ifiss_mat.mat", ...
+    "5_bundle_adj.mat","6_G3_circuit.mat"];
+
 datadir = fullfile("..", "data", "matlab");
 
-m = load(fullfile(datadir, filename));
-m = m.Problem;
+data = [];
+for i = 1:length(names)
+    filename = names(i);
+    m = load(fullfile(datadir, filename));
+    m = m.Problem;
 
-A = sparse(m.A);
-clear("m");
+    A = sparse(m.A);
+    clear("m");
 
-[N, err, t] = solve(A);
-fprintf("%d, %e, %f, \n", N, err, t);
+    [N, err, t] = solve(A);
+    data = [data; [N, err, t]];
+    fprintf("%d, %e, %f, \n", N, err, t);
+end
+
+output_path = fullfile("output", strcat(os, ".csv"));
+T = array2table(data);
+T.Properties.VariableNames(1:3) = ["N","Error", "Time"];
+writetable(T, output_path);
 
 function [N, err, time] = solve(A)
     N = size(A, 1);
