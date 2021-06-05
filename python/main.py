859dccba3dc1e79a646458ce39032aef5c02fa89
Giuseppe <giuseppe.magazu@gmail.com>
2021-06-06 00:20:53 +0200

    remove row header option when saving the csv

--- python/main.py
+++ python/main.py
@@ -2,7 +2,8 @@ import os
 import time
 import psutil
 import platform
-#import tracemalloc
+
+# import tracemalloc
 
 import numpy as np
 import pandas as pd
@@ -22,13 +23,13 @@ def solve(A):
     b = A.dot(e)
 
     start = time.time()
-    #tracemalloc.start()
+    # tracemalloc.start()
 
     try:
         factor = cholesky(A)
         x = factor(b)
     except CholmodNotPositiveDefiniteError:
-        print('not positive matrix')
+        print("not positive matrix")
         x = spsolve(A, b).reshape([N, 1])
 
     end = time.time()
@@ -37,17 +38,18 @@ def solve(A):
 
     memory = psutil.virtual_memory().used + psutil.swap_memory().used
 
-    #current, peak = tracemalloc.get_traced_memory()
-    #snapshot = tracemalloc.take_snapshot()
-    #for stat in snapshot.statistics("lineno"):
+    # current, peak = tracemalloc.get_traced_memory()
+    # snapshot = tracemalloc.take_snapshot()
+    # for stat in snapshot.statistics("lineno"):
     #    print(stat)
-    #tracemalloc.stop()
+    # tracemalloc.stop()
 
     return N, error, elapsed, memory
 
+
 def main():
     cwd = os.getcwd()
-    curr_os = platform.system().lower() #os.uname()[0].lower()
+    curr_os = platform.system().lower()  # os.uname()[0].lower()
     data_dir = os.path.join(cwd, "..", "data", "matrix_market")
 
     data = []
@@ -65,21 +67,21 @@ def main():
 
     df = pd.DataFrame(data)
     output_path = os.path.join("output", "{}.csv".format(curr_os))
-    df.to_csv(output_path)
+    df.to_csv(output_path, index = False)
+
 
 def test():
-	cwd = os.getcwd()
-	data_dir = os.path.join(cwd, "..", "data", "matrix_market")
-	f = "7_Hook_1498.mtx"
-	filename = os.path.join(data_dir, f)
-	
-	print(f"Loading {f}...")
-	A = mmread(filename).tocsc()
-	print("Loaded.")
+    cwd = os.getcwd()
+    data_dir = os.path.join(cwd, "..", "data", "matrix_market")
+    f = "7_Hook_1498.mtx"
+    filename = os.path.join(data_dir, f)
 
-	N, error, time, space = solve(A)
-	print({"N": N, "Time": time, "Error": error, "Space": space})
+    print(f"Loading {f}...")
+    A = mmread(filename).tocsc()
+    print("Loaded.")
 
+    N, error, time, space = solve(A)
+    print({"N": N, "Time": time, "Error": error, "Space": space})
 
 
 if __name__ == "__main__":
