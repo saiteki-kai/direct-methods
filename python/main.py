f2fd7c67837a9d1cf77c5f5d69b8336ec471797b
Magazzù Gaetano <lordfighterx@gmail.com>
2021-06-10 20:30:48 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -14,6 +14,10 @@ from scipy.linalg import norm
 from scipy.sparse.linalg import spsolve, eigs, eigsh
 from sksparse.cholmod import cholesky, CholmodNotPositiveDefiniteError
 
+cwd = os.getcwd()
+curr_os = platform.system().lower()
+data_dir = os.path.join(cwd, "..", "data", "matrix_market")
+
 def solve(A):
   N = A.shape[0]
   e = np.ones([N, 1])
@@ -29,20 +33,14 @@ def solve(A):
     x = spsolve(A, b).reshape([N, 1])
 
   end = time.time()
-  memory = psutil.Process().memory_info().rss
+  space = psutil.Process().memory_info().rss
 
   elapsed = end - start
   error = norm(x - e) / norm(e)
 
-  return N, error, elapsed, memory
-
+  return N, error, elapsed, space
 
 def main():
-    cwd = os.getcwd()
-    curr_os = platform.system().lower()
-    process = psutil.Process(os.getpid())
-    data_dir = os.path.join(cwd, "..", "data", "matrix_market")
-
     data = []
     for f in sorted(os.listdir(data_dir)):
         filename = os.path.join(data_dir, f)
@@ -60,43 +58,16 @@ def main():
     output_path = os.path.join("output", "{}.csv".format(curr_os))
     df.to_csv(output_path, index = False)
 
-
 def test():
-  cwd = os.getcwd()
-  data_dir = os.path.join(cwd, "..", "data", "matrix_market")
-  f = "7_Hook_1498.mtx"
+  f = "2_ns3Da.mtx"
   filename = os.path.join(data_dir, f)
-  
+
   print(f"Loading {f}...")
   A = mmread(filename).tocsc()
   print("Loaded.")
-  
+
   N, error, time, space = solve(A)
   print({"N": N, "Time": time, "Error": error, "Space": space})
-
-def mem_test():
-  N = 6
-  #tracemalloc.start()
-  a = np.random.rand(N*2**27)
-  """
-  snapshot = tracemalloc.take_snapshot()
-  top_stats = snapshot.statistics('lineno')
-  print('[Top 10]')
-  for stat in top_stats[:10]:
-    print(stat)
-  _, peak = tracemalloc.get_traced_memory()
-  #tracemalloc.reset_peak()
-  tracemalloc.stop()
-  print(peak)
-  """
-
-"""
-memory_start = psutil.Process().memory_full_info().uss
-a = np.random.rand(N*2**27)
-memory_end = psutil.Process().memory_full_info().uss
-print("memory-start: {} bytes".format(memory_start))
-print("memory-end: {} bytes".format(memory_end))
-print("memory: {} bytes".format(memory_end - memory_start))"""
 
 
 if __name__ == "__main__":
